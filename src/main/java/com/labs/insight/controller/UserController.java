c028440b7738d1833e8dad16783ca4e15c34cd6a
Adalberto Hernandez [CO] <n316570@ad.btmna.com>
2020-03-18 17:36:04 -0600

    working service

--- src/main/java/com/labs/insight/controller/UserController.java
+++ src/main/java/com/labs/insight/controller/UserController.java
@@ -1,26 +1,26 @@
 package com.labs.insight.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.labs.insight.service.UserService;
+import com.labs.insight.repository.UserRepository;
 
 @RestController
-@RequestMapping(path = "/insight/")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4201")
 public class UserController {
 
-	@Autowired
-	private UserService uService;
+	private final UserRepository userRepo;
 
-	@RequestMapping(path = "/getUsers/")
+	public UserController(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
+
+	@GetMapping(path = "/getUsers")
 	public ResponseEntity<?> getUsers() {
-		return ResponseEntity.status(HttpStatus.OK).body(uService.getUsers());
-
+		return ResponseEntity.status(HttpStatus.OK).body(userRepo.findAll());
 	}
 
 }
--- src/main/java/com/labs/insight/service/UserServiceImpl.java
+++ src/main/java/com/labs/insight/service/UserServiceImpl.java
@@ -2,23 +2,17 @@ package com.labs.insight.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.labs.insight.entity.UserEntity;
-import com.labs.insight.repository.UserResository;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-	@Autowired
-	private UserResository userRepo;
-
 	@Override
 	public List<UserEntity> getUsers() {
 		// TODO Auto-generated method stub
-
-		return userRepo.findAll();
+		return null;
 	}
 
 }
